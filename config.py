5608e475868ebba37c62eee45a323fb5c7d91dd1
Sri Aspari <sriaspari@gmail.com>
2020-08-21 00:51:21 +0700

    Microblog: Database

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 import os
 
+basepath = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'write-your-key-here'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basepath, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
