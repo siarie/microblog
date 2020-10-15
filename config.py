9158542857679d2b464c241424e9f94f5eb22bf5
Sri Aspari <sriaspari@gmail.com>
2020-10-15 18:51:08 +0700

    Microblog: Error Handling [Not Complete]

--- config.py
+++ config.py
@@ -7,3 +7,9 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'write-your-key-here'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basepath, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_LTS = os.environ.get('MAIL_USE_LTS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    ADMINS = ['sriaspari@gmail.com']
